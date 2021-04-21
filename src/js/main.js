3b4431217055743be1aac47f9c317633a1801ccc
Andrea Gori <Andrea.fr.gori@gmail.com>
2021-04-21 19:07:55 +0200

    nav completa

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+  el: '#root',
+  data:{
+    collegamenti: [
+      {
+        name: 'Order Online'
+      },
+      {
+        name: 'About'
+      },
+      {
+        name:'News'
+      },
+      {
+        name: ' Contact Us'
+      }
+    ]
+  },
+  methods:{
+
+  },
+})
---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+let mix = require('laravel-mix');
+ mix.js('src/js/main.js', 'js')
+ .sass('src/master.scss','css')
+ .setPublicPath('assets')
+ .options({
+    processCssUrls: false
+});
