f3fb769d5d699fa75c186772353002080f28ae4e
Andrea Gori <Andrea.fr.gori@gmail.com>
2021-04-23 10:31:10 +0200

    metà main svolto

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,6 +18,19 @@ var app = new Vue({
       name: 'News'
     }, {
       name: ' Contact Us'
+    }],
+    popularDishes: [{
+      name: "./assets/img/skin-on-fries.jpg",
+      title: 'Skin On Fries',
+      price: '$3.00 – $6.00'
+    }, {
+      name: "./assets/img/choco-cookie-frappe.jpg",
+      title: 'Choco Cookie Frappe',
+      price: '$4.99'
+    }, {
+      name: "./assets/img/donut-burger.jpg",
+      title: 'The Donut Burger',
+      price: '$6.99'
     }]
   },
   methods: {}
--- src/js/main.js
+++ src/js/main.js
@@ -14,6 +14,24 @@ var app = new Vue({
       {
         name: ' Contact Us'
       }
+    ],
+
+    popularDishes:[
+      {
+        name: "./assets/img/skin-on-fries.jpg",
+        title: 'Skin On Fries',
+        price: '$3.00 – $6.00'
+      },
+      {
+        name: "./assets/img/choco-cookie-frappe.jpg",
+        title: 'Choco Cookie Frappe',
+        price: '$4.99'
+      },
+      {
+        name: "./assets/img/donut-burger.jpg",
+        title: 'The Donut Burger',
+        price: '$6.99'
+      },
     ]
   },
   methods:{
